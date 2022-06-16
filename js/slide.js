1dfc88259564dc133462a70f7fe49bfac3e35c2b
Elena-VN <43069050+Elena-VN@users.noreply.github.com>
2022-06-16 15:57:50 +0300

    photo slider

---
+++ js/slide.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	swiper: Swiper,
+	breakpoints: {
+		768: {
+			slidesPerView: 1.76,
+			spaceBetween: 10
+		},
+		880: {
+			slidesPerView: 2,
+			spaceBetween: 10
+		},
+		920: {
+			slidesPerView: 2.1,
+			spaceBetween: 10
+		},
+		1020: {
+			slidesPerView: 2.3,
+			spaceBetween: 10
+		},
+		1080: {
+			slidesPerView: 2.4,
+			spaceBetween: 20
+		}
+	},
+	loop: true,
+	centeredSlides: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	}
+})
+
